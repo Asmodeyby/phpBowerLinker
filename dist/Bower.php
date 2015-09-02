72e59583c5902ddf6e247c8bfe40efb532527dde
Asmodeyby <Asmodey2007@tut.by>
2015-09-03 00:00:36 +0300

    Fix few moments

--- dist/Bower.php
+++ dist/Bower.php
@@ -10,6 +10,11 @@ class Helper_Bower {
     private $_prepared_assets = array();
     private $_raw_assets = array();
 
+    /**
+     * @param $bower_path - bower.json path
+     * @param $bower_components_path - bower_components path
+     * @throws Exception
+     */
     function __construct($bower_path, $bower_components_path) {
         if (file_exists($bower_path)) {
             $this->bower_path = $bower_path;
@@ -19,6 +24,11 @@ class Helper_Bower {
         }
     }
 
+    /**
+     * Get all dependencies from main bower.json and prepare children bower.json.
+     *
+     * @return array
+     */
     function getAssets() {
         $this->bower_json = json_decode(file_get_contents($this->bower_path), true);
         if (isset($this->bower_json['dependencies'])) {
@@ -47,13 +57,24 @@ class Helper_Bower {
         return $this->_raw_assets;
     }
 
+    /**
+     * Get pathinfo for all our assets
+     *
+     * @return array
+     */
     function prepareAssets() {
         foreach($this->_raw_assets as $currentAsset) {
             $info = pathinfo($currentAsset);
             $this->_prepared_assets[] = $info;
         }
+        return $this->_prepared_assets;
     }
 
+    /**
+     * Process and build html tags for js and css
+     *
+     * @return array - return compiled assets array
+     */
     function processAssets() {
         foreach($this->_prepared_assets as $currentAsset) {
             if ($currentAsset["extension"]=="js") {
